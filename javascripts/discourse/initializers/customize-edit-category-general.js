5935fd288228da0cce5dca89da875cd5611d10e4
neounix <neo@unix.com>
2025-04-19 03:19:23 +0700

    Update customize-edit-category-general.js

--- javascripts/discourse/initializers/customize-edit-category-general.js
+++ javascripts/discourse/initializers/customize-edit-category-general.js
@@ -1,26 +1,11 @@
-// import { cancel } from "@ember/runloop";
-// import { withPluginApi } from "discourse/lib/plugin-api";
+import { withPluginApi } from "discourse/lib/plugin-api";
 
-// export default {
-//   name: "customize-edit-category-general",
+export default {
+  name: "customize-edit-category-general",
 
-//   initialize() {
-//     withPluginApi("0.8.14", (api) => {
-//       // api.modifyClass("component:edit-category-general", {
-//       //   pluginId: "discourse-air",
-
-//       //   didInsertElement() {
-//       //     this._super(...arguments);
-//       //     document.body.classList.add("edit-category");
-//       //     this._focusCategoryName();
-//       //   },
-
-//       //   willDestroyElement() {
-//       //     this._super(...arguments);
-//       //     document.body.classList.remove("edit-category");
-//       //     this._laterFocus && cancel(this._laterFocus);
-//       //   },
-//       // });
-//     });
-//   },
-// };
+  initialize() {
+    withPluginApi("0.8.14", (api) => {
+      // admin-only component customization temporarily disabled
+    });
+  },
+};
