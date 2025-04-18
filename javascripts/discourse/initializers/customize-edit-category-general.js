4227c0550aa59e279c69f24c3736f985b88fcfac
neounix <neo@unix.com>
2025-04-19 03:17:55 +0700

    Update customize-edit-category-general.js

--- javascripts/discourse/initializers/customize-edit-category-general.js
+++ javascripts/discourse/initializers/customize-edit-category-general.js
@@ -1,28 +1,26 @@
-/* import { cancel } from "@ember/runloop"; */
-import { withPluginApi } from "discourse/lib/plugin-api";
+// import { cancel } from "@ember/runloop";
+// import { withPluginApi } from "discourse/lib/plugin-api";
 
-export default {
-  name: "customize-edit-category-general",
+// export default {
+//   name: "customize-edit-category-general",
 
-  initialize() {
-    withPluginApi("0.8.14", (api) => {
-      /*
-      api.modifyClass("component:edit-category-general", {
-        pluginId: "discourse-air",
+//   initialize() {
+//     withPluginApi("0.8.14", (api) => {
+//       // api.modifyClass("component:edit-category-general", {
+//       //   pluginId: "discourse-air",
 
-        didInsertElement() {
-          this._super(...arguments);
-          document.body.classList.add("edit-category");
-          this._focusCategoryName();
-        },
+//       //   didInsertElement() {
+//       //     this._super(...arguments);
+//       //     document.body.classList.add("edit-category");
+//       //     this._focusCategoryName();
+//       //   },
 
-        willDestroyElement() {
-          this._super(...arguments);
-          document.body.classList.remove("edit-category");
-          this._laterFocus && cancel(this._laterFocus);
-        },
-      });
-      */
-    });
-  },
-};
+//       //   willDestroyElement() {
+//       //     this._super(...arguments);
+//       //     document.body.classList.remove("edit-category");
+//       //     this._laterFocus && cancel(this._laterFocus);
+//       //   },
+//       // });
+//     });
+//   },
+// };
